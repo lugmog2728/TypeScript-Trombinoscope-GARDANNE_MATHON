d005d0d08a669a69c48efd481c852dd4c3fd7348
Tokinoi <alonzo.mathon@outlook.com>
2025-02-24 14:26:07 +0100

    add first pages and directory structure

--- src/app.ts
+++ src/app.ts
@@ -6,7 +6,7 @@ const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-    res.send('Hello, toto with TypeScript!');
+    res.sendFile(__dirname + '/views/index.html');
 });
 
 app.listen(port, () => {
